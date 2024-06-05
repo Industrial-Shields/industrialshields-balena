12fb504b2cd73f975bf925cefe62c6a71261a84b
Marc Pous <marc.pous@gmail.com>
2024-06-05 13:53:08 +0200

    Working with node-RED

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -2,17 +2,6 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-# rpiplc hw-config 
-# ExecStartPre equivalent: Wait for /dev/ttySC* to exist
-while [ ! -c /dev/ttySC* ]; do 
-    sleep 1
-done
-sleep 1
-
-# ExecStart equivalent: Run the hw-config command
-/usr/local/bin/hw-config
-
-
 # Make sure ssh known hosts file is present
 if ! test -f /data/known_hosts;
     then
